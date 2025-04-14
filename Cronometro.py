1df5ac87a6e07fb6def5246778752bae2de4bd72
brian <brianarguello.vga>
2025-04-13 22:26:26 -0300

    Experimentado con Qlabel

--- Cronometro.py
+++ Cronometro.py
@@ -1,9 +1,21 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
 def My_app():
+    #Bloque donde agregamos la ventana   
     window = QWidget()
     window.setWindowTitle("Cronometro")
     window.resize(400, 300)
+    
+    #Crear el primer layout o como yo lo conozco mejor primer div/contenedor
+    firstLayout = QVBoxLayout()
+
+    #Ahora creare un label 
+    label = QLabel("primer layout")
+    firstLayout.addWidget(label)
+
+    #asignar el contenedor a la ventana
+    window.setLayout(firstLayout)
+
     return window
 
 app = QApplication([])
